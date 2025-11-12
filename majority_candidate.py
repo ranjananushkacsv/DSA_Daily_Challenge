0ccaa9abcb54f2febaf7b6e08f7069b07a851675
ranjananushkacsv <ranjananushka90@gmail.com>
2025-11-12 21:41:17 +0530

    Day-14

--- majority_candidate.py
+++ majority_candidate.py
@@ -8,8 +8,9 @@ def majorityElement(self, nums: List[int]) -> int:
         count += (1 if num == candidate else -1)
     
     return candidate
+
 '''APPROACH -
 1. initially we will have a count and candidate variable
 2. we will traverse array num, if we see the count is zero we add that num to candidate
-3. else if we already have it as candidate then we increment the count by 1
+3. else if we already have it as candidate then we increment the count by 1. Approach
 '''
